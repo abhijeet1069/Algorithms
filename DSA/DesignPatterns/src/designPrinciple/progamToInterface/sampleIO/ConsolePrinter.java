2e123c32b23bd500dcf0c4744b3cd35c82356cab
abhijeet <abhijeet1069@gmail.com>
2024-11-26 08:51:05 +0530

    programming model completed

--- DSA/DesignPatterns/src/designPrinciple/progamToInterface/sampleIO/ConsolePrinter.java
+++
@@ -1,8 +0,0 @@
-package designPrinciple.progamToInterface.sampleIO;
-
-public class ConsolePrinter implements Printer{
-	@Override
-	public void print(String str) {
-		System.out.println(str);
-	}
-}
--- DSA/DesignPatterns/src/designPrinciple/progamToInterface/sampleIO/Printer.java
+++
@@ -1,5 +0,0 @@
-package designPrinciple.progamToInterface.sampleIO;
-
-public interface Printer {
-	public void print(String str);
-}
